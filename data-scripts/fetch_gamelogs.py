753c103d2fce9ecbdc2c65457734950dda07a428
Evan Feng <evanfeng55@gmail.com>
2016-07-13 20:46:33 -0400

    modified fetch_gamelogs to skip over players whose gamelogs are already downloaded

--- data-scripts/fetch_gamelogs.py
+++ data-scripts/fetch_gamelogs.py
@@ -1,5 +1,5 @@
 # USAGE:
-# python fetch_gamelogs.py [season] [player name (underscore separated)] 
+# python fetch_gamelogs.py [player name (underscore separated)] 
 
 # EXAMPLE python fetch_gamelogs.py 2015-16 steph_curry
 
@@ -9,155 +9,187 @@ import json
 import urllib2
 from bs4 import BeautifulSoup
 import re
+import os.path
 
 
+##### function for resolving names #####
+def matchNames(player_name):
+
+	playername = player_name.split(' ')[1][0:5] + player_name.split(' ')[0][0:2] + "01"
+	
+	if(player_name == 'jeff ayres'):
+		playername = 'pendeje02'
+	elif(player_name == 'harrison barnes'):
+		playername = 'barneha02'
+	elif(player_name== 'matt barnes'):
+		playername = 'barnema02'
+	elif(player_name == 'bojan bogdanovic'):
+		playername = 'bogdabo02'
+	elif(player_name == 'anthony brown'):
+		playername = 'brownan02'
+	elif(player_name == 'markel brown'):
+		playername = 'brownma02'
+	elif(player_name == 'clint capela'):
+		playername = 'capelca01'
+	elif(player_name == 'anthony davis'):
+		playername = 'davisan02'
+	elif(player_name == 'mike dunleavy'):
+		playername = 'dunlemi02'
+	elif(player_name == 'danny green'):
+		playername = 'greenda02'
+	elif(player_name == 'jeff green'):
+		playername = 'greenje02'
+	elif(player_name == 'pj hairston'):
+		playername = 'hairspj02'
+	elif(player_name == 'jordan hamilton'):
+		playername = 'hamiljo02'
+	elif(player_name == 'tim hardaway'):
+		playername = 'hardati02'
+	elif(player_name == 'tobias harris'):
+		playername = 'harrito02'
+	elif(player_name == 'gerald henderson'):
+		playername = 'hendege02'
+	elif(player_name == 'john holland'):
+		playername = 'hollajo02'
+	elif(player[6] == 'christapher_johnson'):
+		playername = 'johnsch04'
+	elif(player_name == 'joe johnson'):
+		playername = 'johnsjo02'
+	elif(player_name == 'stanley johnson'):
+		playername = 'johnsst04'
+	elif(player_name == 'dahntay jones'):
+		playername = 'jonesda02'
+	elif(player_name == 'james jones'):
+		playername = 'jonesja02'
+	elif(player_name == 'brandon knight'):
+		playername = 'knighbr03'
+	elif(player_name == 'david lee'):
+		playername = 'leeda02'
+	elif(player_name == 'kevin martin'):
+		playername = 'martike02'
+	elif(player_name == 'wesley matthews'):
+		playername = 'matthwe02'
+	elif(player_name == 'andre miller'):
+		playername = 'millean02'
+	elif(player_name == 'markieff morris'):
+		playername = 'morrima02'
+	elif(player_name == 'marcus morris'):
+		playername = 'morrima03'
+	elif(player_name == 'xavier munford'):
+		playername = 'munfoxa02'
+	elif(player_name == 'larry nance'):
+		playername = 'nancela02'
+	elif(player_name == 'willie reed'):
+		playername = 'reedwi02'
+	elif(player_name == 'andre roberson'):
+		playername = 'roberan03'
+	elif(player_name == 'glenn robinson'):
+		playername = 'robingl02'
+	elif(player_name == 'jakarr sampson'):
+		playername = 'sampsja02'
+	elif(player_name == 'jonathon simmons'):
+		playername = 'simmojo02'
+	elif(player_name == 'greg smith'):
+		playername = 'smithgr02'
+	elif(player_name == 'jason smith'):
+		playername = 'smithja02'
+	elif(player_name == 'josh smith'):
+		playername = 'smithjo03'
+	elif(player_name == 'isaiah thomas'):
+		playername = 'thomais02'
+	elif(player_name == 'jason thompson'):
+		playername = 'thompja02'
+	elif(player_name == 'kemba walker'):
+		playername = 'walkeke02'
+	elif(player_name == 'alan williams'):
+		playername = 'willial03'
+	elif(player_name == 'lou williams'):
+		playername = 'willilo02'
+	elif(player_name == 'marcus williams'):
+		playername = 'willima04'
+	elif(player_name == 'marvin williams'):
+		playername = 'willima02'
+	elif(player_name == 'mo williams'):
+		playername = 'willima01'
+	elif(player_name == 'metta world peace'):
+		playername = 'artesro01'
+	elif(player_name == 'brandan wright'):
+		playername = 'wrighbr03'
+
+	return playername;
+
+##### end matchNames() function #####
+
+
+##### function to format name #####
+def formatName(name):
+
+	player_name = None;
+
+	# remove "jr" from any player names
+	if(name[-3:] == 'jr.'):
+		player_name = name[:-4]
+
+	# remove special chars from player names
+	player_name = re.sub('[!@#$-.]', '', name)
+
+	return player_name
+
+##### end formatName() function
+
+
+# user provided arguments
 endyr = sys.argv[1][5:]
-#Detects for multiple years
+
+if(len(sys.argv) == 3):
+	name = sys.argv[2]
+else:
+	name = None
+
+
+
+# loop through each year specified in the range
 while (int(sys.argv[1][2:4]) < int(endyr)):
+
 	season = '20'+str(int(endyr)-1)+'-'+str(endyr)
-	#print(season)
+
+	# get activeplayers list from the activeplayers_file (stored locally)
 	with open('../data-local/activeplayers/activeplayers_' + season + '.json') as activeplayers_file:
 		activeplayers = json.load(activeplayers_file)
 
-	start = False
-	if (len(sys.argv) > 2):
-		start=False
-		name = sys.argv[2]
-	else:
-		start=True
-		name = ''
 
-	# loop through the activeplayers file, take the team info and add team/other info for each player obj
+	# loop through all players in the activeplayers file
 	for player in activeplayers['resultSets'][0]['rowSet']:
 
-		playName = player[2].lower()
-		playerID = player[0]
 
-		if(playName[-3:] == 'jr.'):
-			playName = playName[:-4]
-		
-		playName = re.sub('[!@#$-.]', '', playName)
+		player_id = player[0]
+		player_name = player[2].lower()
 
-		if(start or playName == (name.split('_')[0] + ' ' +  name.split('_')[1]) ):
-			start = True
-			if(playName == 'nene'):
-				playName = 'nene hilario'
-			elif(playName == 'jose juan barea'):
-				playName = 'josejuan barea'
-			elif(playName == 'luc mbah a moute'):
-				playName = 'luc mbahamoute'
-			elif(playName == 'james michael mcadoo'):
-				playName = 'jamesmichael mcadoo'
+
+		# check if file already exists in filesystem for this player
+		if not os.path.isfile("../data-local/gamelogs/" + str(player_id) + ".json"):
+
+			player_name = formatName(player_name)
+
+			# hard coded player name cases
+			if(player_name == 'nene'):
+				player_name = 'nene hilario'
+			elif(player_name == 'jose juan barea'):
+				player_name = 'josejuan barea'
+			elif(player_name == 'luc mbah a moute'):
+				player_name = 'luc mbahamoute'
+			elif(player_name == 'james michael mcadoo'):
+				player_name = 'jamesmichael mcadoo'
 			
-			playername = playName.split(' ')[1][0:5] + playName.split(' ')[0][0:2] + "01"
+			playername = matchNames(player_name)
 			
-			if(playName == 'jeff ayres'):
-				playername = 'pendeje02'
-			elif(playName == 'harrison barnes'):
-				playername = 'barneha02'
-			elif(playName== 'matt barnes'):
-				playername = 'barnema02'
-			elif(playName == 'bojan bogdanovic'):
-				playername = 'bogdabo02'
-			elif(playName == 'anthony brown'):
-				playername = 'brownan02'
-			elif(playName == 'markel brown'):
-				playername = 'brownma02'
-			elif(playName == 'clint capela'):
-				playername = 'capelca01'
-			elif(playName == 'anthony davis'):
-				playername = 'davisan02'
-			elif(playName == 'mike dunleavy'):
-				playername = 'dunlemi02'
-			elif(playName == 'danny green'):
-				playername = 'greenda02'
-			elif(playName == 'jeff green'):
-				playername = 'greenje02'
-			elif(playName == 'pj hairston'):
-				playername = 'hairspj02'
-			elif(playName == 'jordan hamilton'):
-				playername = 'hamiljo02'
-			elif(playName == 'tim hardaway'):
-				playername = 'hardati02'
-			elif(playName == 'tobias harris'):
-				playername = 'harrito02'
-			elif(playName == 'gerald henderson'):
-				playername = 'hendege02'
-			elif(playName == 'john holland'):
-				playername = 'hollajo02'
-			elif(player[6] == 'christapher_johnson'):
-				playername = 'johnsch04'
-			elif(playName == 'joe johnson'):
-				playername = 'johnsjo02'
-			elif(playName == 'stanley johnson'):
-				playername = 'johnsst04'
-			elif(playName == 'dahntay jones'):
-				playername = 'jonesda02'
-			elif(playName == 'james jones'):
-				playername = 'jonesja02'
-			elif(playName == 'brandon knight'):
-				playername = 'knighbr03'
-			elif(playName == 'david lee'):
-				playername = 'leeda02'
-			elif(playName == 'kevin martin'):
-				playername = 'martike02'
-			elif(playName == 'wesley matthews'):
-				playername = 'matthwe02'
-			elif(playName == 'andre miller'):
-				playername = 'millean02'
-			elif(playName == 'markieff morris'):
-				playername = 'morrima02'
-			elif(playName == 'marcus morris'):
-				playername = 'morrima03'
-			elif(playName == 'xavier munford'):
-				playername = 'munfoxa02'
-			elif(playName == 'larry nance'):
-				playername = 'nancela02'
-			elif(playName == 'willie reed'):
-				playername = 'reedwi02'
-			elif(playName == 'andre roberson'):
-				playername = 'roberan03'
-			elif(playName == 'glenn robinson'):
-				playername = 'robingl02'
-			elif(playName == 'jakarr sampson'):
-				playername = 'sampsja02'
-			elif(playName == 'jonathon simmons'):
-				playername = 'simmojo02'
-			elif(playName == 'greg smith'):
-				playername = 'smithgr02'
-			elif(playName == 'jason smith'):
-				playername = 'smithja02'
-			elif(playName == 'josh smith'):
-				playername = 'smithjo03'
-			elif(playName == 'isaiah thomas'):
-				playername = 'thomais02'
-			elif(playName == 'jason thompson'):
-				playername = 'thompja02'
-			elif(playName == 'kemba walker'):
-				playername = 'walkeke02'
-			elif(playName == 'alan williams'):
-				playername = 'willial03'
-			elif(playName == 'lou williams'):
-				playername = 'willilo02'
-			elif(playName == 'marcus williams'):
-				playername = 'willima04'
-			elif(playName == 'marvin williams'):
-				playername = 'willima02'
-			elif(playName == 'mo williams'):
-				playername = 'willima01'
-			elif(playName == 'metta world peace'):
-				playername = 'artesro01'
-			elif(playName == 'brandan wright'):
-				playername = 'wrighbr03'
-			
+
 			# parse arguments
-			displayname = playName.split(' ')[0].title() + " " + playName.split(' ')[1].title()
+			displayname = player_name.split(' ')[0].title() + " " + player_name.split(' ')[1].title()
 			season_endyr = '20'+str(endyr)
-
 
 			print("PLAYERNAME: " + playername + "\n")
 			print("SEASONENDYR: " + season_endyr + "\n")
-
 
 			# (1) construct request url
 			request_url = "http://www.basketball-reference.com/players/" + playername[0] + "/" + playername + "/gamelog/" + season_endyr + "/";
@@ -175,7 +207,6 @@ while (int(sys.argv[1][2:4]) < int(endyr)):
 
 			# (4) iterate over rows and get cell values
 			print("length: " + str(len(rows)) + "\n")
-
 
 			output = {}
 			output['player_name'] = displayname
@@ -201,8 +232,15 @@ while (int(sys.argv[1][2:4]) < int(endyr)):
 				output[sys.argv[1]].append(game_arr)
 
 			# write output to a json file
-			with open('../data-local/gamelogs/' + str(playerID) + '.json', 'w') as outfile:
+			with open('../data-local/gamelogs/' + str(player_id) + '.json', 'w') as outfile:
 				json.dump(output, outfile)
 
+		else:
+			print("FILE ALREADY EXISTS FOR " + str(player_id) + ", SKIPPING")
 
-	endyr =int(endyr)-1
+
+	endyr-=1
+
+
+
+
